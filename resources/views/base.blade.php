daf1ddf7918c20b21a360b06eb96cbb8dcea34f8
Naomi Stevenson <stevenson-n3@ulster.ac.uk>
2020-03-09 13:26:51 +0000

    start of footer

---
+++ resources/views/base.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+<html lang = "en">
+
+<head>
+
+@yield('title')
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+<link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="./resources/sass/base.css">
+
+</head>
+
+<body>
+
+<h1>Hello</h1>
+
+@yield ('content')
+
+<footer>
+
+        <div class = "container">
+
+            <div class = "row">
+                <div class = "col footer-option">
+                    About
+                </div>
+
+                <div class = "col footer-option">
+                    Get In Touch
+                </div>
+                
+                <div class = "col footer-option">
+                    <a href = "https://facebook.com" target = "_blank">
+                        <img class = "socialmedia" alt = "facebook logo" src = "./images/facebook.png">
+                    </a>
+                        
+                    <a href = "https://twitter.com" target = "_blank">
+                        <img class = "socialmedia" alt = "twitter logo" src = "images/twitter.png">
+                    </a>
+
+                    <a href = "https://instagram.com" target = "_blank">
+                        <img class = "socialmedia" alt = "instagram logo" src = "images/instagram.png">
+                    </a>
+
+                    <a href = "https://youtube.com" target = "_blank">
+                        <img class = "socialmedia" alt = "pinterest logo" src = "images/pinterest.png">
+                    </a>
+                </div>
+            
+            </div>
+
+            <p class = "copyright">© Digital Ink. 2020</p>
+        </div>
+            
+    </footer>
+
+</body>
+
+</html>
