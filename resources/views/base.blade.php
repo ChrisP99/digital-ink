85b6f2b9a6a2af6c89ab16026c7f68b10f5f361a
ChrisP99 <perry-c2@ulster.ac.uk>
2020-03-09 13:55:00 +0000

    Fixed broken css

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,11 +6,11 @@
 
 @yield('title')
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"
 
 <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" type="text/css" href="./resources/sass/base.css">
+<link rel="stylesheet" type="text/css" href="css/base.css">
 
 </head>
 
@@ -32,12 +32,12 @@
                 <div class = "col footer-option">
                     Get In Touch
                 </div>
-                
+
                 <div class = "col footer-option">
                     <a href = "https://facebook.com" target = "_blank">
-                        <img class = "socialmedia" alt = "facebook logo" src = "./images/facebook.png">
+                        <img class = "socialmedia" alt = "facebook logo" src = "images/facebook.png">
                     </a>
-                        
+
                     <a href = "https://twitter.com" target = "_blank">
                         <img class = "socialmedia" alt = "twitter logo" src = "images/twitter.png">
                     </a>
@@ -46,16 +46,16 @@
                         <img class = "socialmedia" alt = "instagram logo" src = "images/instagram.png">
                     </a>
 
-                    <a href = "https://youtube.com" target = "_blank">
+                    <a href = "https://pintrest.com" target = "_blank">
                         <img class = "socialmedia" alt = "pinterest logo" src = "images/pinterest.png">
                     </a>
                 </div>
-            
+
             </div>
 
             <p class = "copyright">© Digital Ink. 2020</p>
         </div>
-            
+
     </footer>
 
 </body>
