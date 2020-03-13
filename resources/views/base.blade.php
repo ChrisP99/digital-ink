9bc2994d62935b14408f6c82d419a250d9ee66ec
ChrisP99 <perry-c2@ulster.ac.uk>
2020-03-13 12:56:32 +0000

    Added header from base

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,57 +4,107 @@
 
 <head>
 
-@yield('title')
+    <!-- meta elements -->
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"
+    <meta charset="UTF-8">
+    <meta name="description" content="online book publishing">
+    <meta name="keywords" content="book,author,reader">
+    <meta name="author" content="Team 41">
 
-<link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+    <!-- viewport -->
 
-<link rel="stylesheet" type="text/css" href="css/base.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- additional head information relative to page to be added -->
+
+@yield('additionalHeadInfo')
+
+<!-- styling -->
+
+    <link rel="icon" href="images/digital-ink-logo.png">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="css/base.css">
 
 </head>
 
 <body>
 
-<h1>Hello</h1>
+<!-- page header -->
+
+<header>
+    <div class = "container">
+        <div class = "row">
+            <!-- stories page link -->
+            <div class = "col header-option">
+                <a href="/stories" class = "header-option">Stories</a>
+            </div>
+
+            <!-- homepage link / logo -->
+            <div class = "col header-option">
+                <a href="/">
+                    <img class="logo" alt = "Digital Ink logo" src = "images/digital-ink-logo.png" align="middle"></a>
+            </div>
+
+            <!-- profile page link -->
+            <div class = "col header-option">
+                <a href="/account" class = "header-option">Account</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- additional page content to be added -->
 
 @yield ('content')
 
+<!-- page footer -->
+
 <footer>
-        <div class = "container">
-            <div class = "row">
-                <div class = "col footer-option">
-                    About
-                </div>
-
-                <div class = "col footer-option">
-                    Get In Touch
-                </div>
-
-                <div class = "col footer-option">
-                    <a href = "https://facebook.com" target = "_blank">
-                        <img class = "socialmedia" alt = "facebook logo" src = "images/facebook.png">
-                    </a>
-
-                    <a href = "https://twitter.com" target = "_blank">
-                        <img class = "socialmedia" alt = "twitter logo" src = "images/twitter.png">
-                    </a>
-
-                    <a href = "https://instagram.com" target = "_blank">
-                        <img class = "socialmedia" alt = "instagram logo" src = "images/instagram.png">
-                    </a>
-
-                    <a href = "https://pintrest.com" target = "_blank">
-                        <img class = "socialmedia" alt = "pinterest logo" src = "images/pinterest.png">
-                    </a>
-                </div>
-
+    <div class = "container">
+        <div class = "row">
+            <!-- link to about page -->
+            <div class = "col footer-option">
+                <a href="/about">About</a>
             </div>
 
-            <p class = "copyright">© Digital Ink. 2020</p>
+            <!-- link to contact page -->
+            <div class = "col footer-option">
+                <a href="/contact">Get In Touch</a>
+            </div>
+
+            <!-- link to social media pages -->
+            <div class = "col footer-option">
+                <!-- Facebook -->
+                <a href = "https://facebook.com" target = "_blank">
+                    <img class = "socialmedia" alt = "facebook logo" src = "images/facebook.png">
+                </a>
+
+                <!-- Twitter -->
+                <a href = "https://twitter.com" target = "_blank">
+                    <img class = "socialmedia" alt = "twitter logo" src = "images/twitter.png">
+                </a>
+
+                <!-- Instagram -->
+                <a href = "https://instagram.com" target = "_blank">
+                    <img class = "socialmedia" alt = "instagram logo" src = "images/instagram.png">
+                </a>
+
+                <!-- Pinterest -->
+                <a href = "https://pintrest.com" target = "_blank">
+                    <img class = "socialmedia" alt = "pinterest logo" src = "images/pinterest.png">
+                </a>
+            </div>
         </div>
 
-    </footer>
+        <!-- copyright message -->
+        <p class = "copyright">© Digital Ink. 2020</p>
+    </div>
+
+</footer>
 
 </body>
 
