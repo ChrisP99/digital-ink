a2575e096a7caea8b47327bcbba7864c28021d60
Gcastelli <castelli-g1@ulster.ac.uk>
2020-03-13 13:12:20 +0000

    Fixed login form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,42 +11,55 @@
 
     </div>
 
-    <div id="background">
-        <div id="form-wrapper">
-            <div id="form1">
-                <form action-="{{url('post-login')}}" method="POST">
-                    {{csrf_field()}}
-                    <label for="Email">Email Address</label>
-                    <input type="text" id="inputEmail" name="email" placeholder="Enter Email Address">
-
-                    <label for="Email">Password</label>
-                    <input type="password" id="inputPassword" name="password" placeholder="Enter Password">
-
-                    <input type="submit" value="submit" class="button">
-                </form>
-
-
-            </div><div id="form2">
-                <form>
-
-
-                    <label for="Name"> Name</label>
-                    <input type="text" placeholder="Enter First Name">
-
-                    <label for="Email">Email Address</label>
-                    <input type="email" placeholder="Enter Email Address">
-
-                    <label for="Email">Password</label>
-                    <input type="password" id="inputPassword" name="password" placeholder="Enter Password">
-
-                    <input type="submit" value="Submit" class="button">
-
-
-                </form>
-            </div>
-
+    <form action="action_page.php" method="post">
+        <div class="imgcontainer">
+            <img src="img_avatar2.png" alt="Avatar" class="avatar">
         </div>
-    </div>
+
+        <div class="container">
+            <label for="uname"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <button type="submit">Login</button>
+            <label>
+                <input type="checkbox" checked="checked" name="remember"> Remember me
+            </label>
+        </div>
+
+
+    </form>
+
+    <p> <br /> <br /> <br /> </p>
+
+    <form action="action_page.php" style="border:1px solid #ccc">
+        <div class="container">
+            <h1>Sign Up</h1>
+            <p>Please fill in this form to create an account.</p>
+            <hr>
+
+            <label for="email"><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="email" required>
+
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <label for="psw-repeat"><b>Repeat Password</b></label>
+            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+            <label>
+                <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+            </label>
+
+            <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+            <div class="clearfix">
+                <button type="submit" >Sign Up</button>
+            </div>
+        </div>
+    </form>
 
 
 @endsection
