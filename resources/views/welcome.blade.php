18deb2004b21d766c366eb4ce02a65bc5e93121b
ChrisP99 <perry-c2@ulster.ac.uk>
2020-03-13 13:33:42 +0000

    Fixed login form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,17 +11,14 @@
 
     </div>
 
-    <form action="action_page.php" method="post">
-        <div class="imgcontainer">
-            <img src="img_avatar2.png" alt="Avatar" class="avatar">
-        </div>
-
+    <form action="{{url('post-login')}}" method="POST">
+        {{ csrf_field() }}
         <div class="container">
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
+            <label for="email"><b>Email</b></label>
+            <input type="text" id="inputEmail" placeholder="Enter Email" name="email" required>
 
             <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
+            <input type="password" id="inputPassword" placeholder="Enter Password" name="psw" required>
 
             <button type="submit">Login</button>
             <label>
@@ -34,26 +31,28 @@
 
     <p> <br /> <br /> <br /> </p>
 
-    <form action="action_page.php" style="border:1px solid #ccc">
+    <form action="{{url('post-registration')}}" method="POST" style="border:1px solid #ccc">
+        {{ csrf_field() }}
         <div class="container">
             <h1>Sign Up</h1>
             <p>Please fill in this form to create an account.</p>
             <hr>
 
+            <label for="name"><b>Name</b></label>
+            <input type="text" id="inputName" placeholder="Enter Name" name="name" required>
+
             <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" required>
+            <input type="text" id="inputEmail" placeholder="Enter Email" name="email" required>
 
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
+            <label for="password"><b>Password</b></label>
+            <input type="password" id="inputPassword" placeholder="Enter Password" name="password" required>
 
-            <label for="psw-repeat"><b>Repeat Password</b></label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+            <label for="password-repeat"><b>Repeat Password</b></label>
+            <input type="password" id="inputPassword" placeholder="Repeat Password" name="password-repeat" required>
 
             <label>
                 <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
             </label>
-
-            <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
 
             <div class="clearfix">
                 <button type="submit" >Sign Up</button>
