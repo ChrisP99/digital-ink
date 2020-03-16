b1e00cde21394152948e2dfe2222cb748ed73a7f
Andrew Neill <neill-a14@ulster.ac.uk>
2020-03-16 19:07:34 +0000

    css added for show form

--- resources/views/stories/show.blade.php
+++ resources/views/stories/show.blade.php
@@ -2,24 +2,16 @@
 
 @section('additionalHeadInfo')
     <title>Index | Digital ink.</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/show.css') }}">
 @endsection
 
 @section('content')
     <div class="main-body">
-
-    <h1>Story Details</h1>
-    <div>
-        <a href="/stories">Back</a>
-    </div>
-
-        <strong>Title:</strong>
-        <p>{{ $story->title }}</p>
-        <strong>Author:</strong>
-        <p>{{ $story->author_id }}</p>
-        <strong>Genre:</strong>
-        <p>{{ $story->genre }}</p>
-        <strong>Content:</strong>
-        <p>{{ $story->content }}</p>
-
+    <h1>{{ $story->title }}</h1>
+        <pre class="author">Written by: {{ $story->author_id }}     Genre: {{ $story->genre }}</pre>
+        <p class="content-section">{{ $story->content }}</p>
+        <div>
+            <a class="btn btn-primary" href="/stories">Back</a>
+        </div>
     </div>
     @endsection
