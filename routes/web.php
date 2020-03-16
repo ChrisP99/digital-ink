70d811da2c259a02943708e4b64acd84403d39ad
Naomi Stevenson <stevenson-n3@ulster.ac.uk>
2020-03-16 11:32:23 +0000

    fixed route error

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
 Route::get('/account', function () {
