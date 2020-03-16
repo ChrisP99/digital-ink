4896e0395e2e86bbf03e808c4ad91bbb4966c51e
Naomi Stevenson <stevenson-n3@ulster.ac.uk>
2020-03-16 13:52:54 +0000

    fixed account blade to extend base

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,19 +1,19 @@
-<!DOCTYPE html>
-<html lang = "en">
+@extends('base')
 
-<head>
-    @yield('title')
+
+    @section('additionalHeadInfo')
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"
     <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/base.css">
     <title>Digital Ink.</title>
-</head>
-<h1>Hello</h1>
-@yield ('content')
+    @endsection
 
-<header class="masthead">
-    <div class="banner h-25">
-    </div>
+
+@section('content')
+
+<h1>Hello</h1>
+
+
     <div class="card">
         <div class="card-body">
             Welcome,{{ ucfirst(Auth()->user()->name) }}!
@@ -21,33 +21,8 @@
         <div class="card-body">
             <a class="small" href="{{url('logout')}}">Logout</a>
         </div>
-</header>
-
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col footer-option">
-                About
-            </div>
-            <div class="col footer-option">
-                Get In Touch
-            </div>
-            <div class="col footer-option">
-                <a href="https://facebook.com" target = "_blank">
-                    <img class = "socialmedia" alt = "facebook logo" src="images/facebook.png">
-                </a>
-                <a href="https://twitter.com" target = "_blank">
-                    <img class = "socialmedia" alt = "twitter logo" src="images/twitter.png">
-                </a>
-                <a href="https://instagram.com" target = "_blank">
-                    <img class="socialmedia" alt = "instagram logo" src="images/instagram.png">
-                </a>
-                <a href="https://pintrest.com" target = "_blank">
-                    <img class="socialmedia" alt = "pinterest logo" src="images/pinterest.png">
-                </a>
-            </div>
-        </div>
-        <p class="copyright">© Digital Ink. 2020</p>
     </div>
-</footer>
-</html>
+
+
+@endsection
+
